c07baf51b85430fc5ab325a381f2a5e091f57f8a
Adam Miskal <adam.nevol@gmail.com>
2022-08-04 16:32:40 +0300

    Add project files.

---
+++ Codewars1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 };
+            Console.WriteLine(Grow(array));
+        }
+
+        public static int Grow(int[] x)
+        {
+            int end = 1;
+            foreach (var number in x)
+            {
+                
+                end *= number;
+            }
+            return end;
+        }
+    }
+   
+}
+
+
